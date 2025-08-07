99b4442e5ad1959e4df2226a9bcff514ab5cad94
Fathimasheril11 <sherilfathima345@gmail.com>
2025-08-07 12:14:45 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,44 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    private int empcode;
+    private String empname;
+    private int empage;
+    private int esalary;
+
+    public int getEmpcode() {
+        return empcode;
+    }
+
+    public void setEmpcode(int empcode) {
+        this.empcode = empcode;
+    }
+
+    public String getEmpname() {
+        return empname;
+    }
+
+    public void setEmpname(String empname) {
+        this.empname = empname;
+    }
+
+    public int getEmpage() {
+        return empage;
+    }
+
+    public void setEmpage(int empage) {
+        this.empage = empage;
+    }
+
+    public int getEsalary() {
+        return esalary;
+    }
+
+    public void setEsalary(int esalary) {
+        this.esalary = esalary;
+    }
+}
